da7f7a8825707b3b0a469b2b5c0bc688e62274e2
vrbprog <vrbdsp@gmail.com>
2024-12-15 16:17:05 +0200

    Add Description block

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,10 @@
+import Description from "./Description/Description";
 
 
 export default function App() {
   return (
     <main>
+      <Description />
     </main>
   );
 }
---
+++ src/components/Description/Description.jsx
@@ -0,0 +1,11 @@
+
+export default function Description() {
+    return (
+        <div>
+            <h2>Sip Happens Café
+            </h2>
+            <p>Please leave your feedback about our service by selecting one of the options below.
+            </p>
+        </div>
+    )
+}
